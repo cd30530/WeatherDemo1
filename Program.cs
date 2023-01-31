99e95141448f90b77fb37c1d0ee69314ad87e7a7
cd30530 <30530@163.com>
2023-01-31 22:47:19 +0800

    test

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    return "welcome 3";
+    return "welcome 3456";
     //var forecast = Enumerable.Range(1, 5).Select(index =>
     //    new WeatherForecast
     //    (
